ce650f8763859058d73ebed95f787d3d1a8d6e67
JD <richardson0705@yahoo.com>
2025-03-18 19:00:27 -0400

    .

---
+++ DR - CyberFu Part 2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxPlayerHealth = 10;
+    public int currentPlayerHealth;
+    public int enemyDamage = 2;
+   
+    void Start()
+    {
+        currentPlayerHealth = maxPlayerHealth;
+    }
+
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamage;
+    }
+    
+}
