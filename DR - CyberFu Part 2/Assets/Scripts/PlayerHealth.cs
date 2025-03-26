6efd09761db109147e0e0ec4d273eb0443fce763
d <richardson0705@yahoo.com>
2025-03-26 18:20:16 -0400

    .

--- DR - CyberFu Part 2/Assets/Scripts/PlayerHealth.cs
+++ DR - CyberFu Part 2/Assets/Scripts/PlayerHealth.cs
@@ -8,15 +8,27 @@ public class PlayerHealth : MonoBehaviour
     public int maxPlayerHealth = 10;
     public int currentPlayerHealth;
     public int enemyDamage = 2;
-   
+
+    private Animator playerAnimator;
     void Start()
     {
         currentPlayerHealth = maxPlayerHealth;
+
+        playerAnimator = GetComponent<Animator>();
     }
 
     public void HurtPlayer()
     {
         currentPlayerHealth -= enemyDamage;
+        playerAnimator.SetTrigger("Hit");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HitCollider"))
+        {
+            HurtPlayer();
+        }
     }
     
 }
